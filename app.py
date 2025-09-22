9ce5419b75b880d2c82ca9b27a4384190eef0bec
SunilB8694 <ep.sunilbhosale8694@gmail.com>
2025-09-22 18:29:41 +0530

    Add Search Api

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from grafityGet import grafity_bp
+from grafityMain import grafitymain_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(grafity_bp)
+app.register_blueprint(grafitymain_bp)
 
 if __name__=='__main':app.run()
